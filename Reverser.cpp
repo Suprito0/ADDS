f7716d7aa3de83cb0e23231a7dfa8f2d26f13134
Suprito Iqbal <a1950138@ingb16w029.ad.adelaide.edu.au>
2025-08-13 17:39:11 +0930

    prac3

--- Reverser.cpp
+++ Reverser.cpp
@@ -1,14 +1,18 @@
 #include"Reverser.h"
 
 int Reverser::reverseDigit(int value){
-    int count = log10(value);
-    cout << "Count: " << count << endl;
     if (value < 10){
         return value;
     }
     else {
-        int s = pow(10,count);
+        int s = pow(10,log10(value));
         return  10 * reverseDigit(value%s) + value/ s;
 
+    }
+}
+
+string Reverser::reverseString(string characters){
+    if (sizeof(characters) == 1) {
+        return characters;
     }
 }
