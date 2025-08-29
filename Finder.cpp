b89b59af2efd56ecdc1c65c77469ad2fddbb8dfa
Suprito <a1950138@adelaide.edu.au>
2025-08-29 15:47:08 +0930

    prac4

--- Finder.cpp
+++ Finder.cpp
@@ -1,42 +1,14 @@
 #include "Finder.h"
 using namespace std;
-// vector<int> Finder::findSubstrings(string s1, string s2) {
-// vector<int> result;
-// for(size_t i = 1; i <= s2.size(); i++) {
-// size_t found = s1.find(s2.substr(0, i));
-// if (found != string::npos) {
-// result.push_back(found);
-// } else {
-// result.push_back(-1);
-// }
-// }
-// return result;
-// }
-vector<int> Finder::findSubstrings(string s1, string s2)
-{
-    vector<int> result;
-    for (size_t i = 1; i <= s2.size(); i++)
-    {
-        size_t found;
-        if (!result.empty())
-        {
-            found = s1.find(s2.substr(0, i), result.back());
-        }
-        else
-        {
-            found = s1.find(s2.substr(0, i));
-        }
-        if (found != string::npos)
-        {
-            result.push_back(found);
-        }
-        else
-        {
-            for (size_t j=i; j<=s2.size(); j++){
-                result.push_back(-1);
-            }
-            break;
-        }
-    }
-    return result;
+vector<int> Finder::findSubstrings(string s1, string s2) {
+vector<int> result;
+for(size_t i = 1; i <= s2.size(); i++) {
+size_t found = s1.find(s2.substr(0, i));
+if (found != string::npos) {
+result.push_back(found);
+} else {
+result.push_back(-1);
+}
+}
+return result;
 }
