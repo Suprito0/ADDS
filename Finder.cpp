58fe646ce6164163c4285680c73cb6038e3bec21
Suprito <a1950138@adelaide.edu.au>
2025-08-29 15:44:29 +0930

    prac4

--- Finder.cpp
+++ Finder.cpp
@@ -18,7 +18,7 @@ vector<int> Finder::findSubstrings(string s1, string s2)
     for (size_t i = 1; i <= s2.size(); i++)
     {
         size_t found;
-        if (result.empty())
+        if (!result.empty())
         {
             found = s1.find(s2.substr(0, i), result.back());
         }
