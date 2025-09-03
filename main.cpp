2142d61e9b09f046056baed3185be980dca03fae
Suprito Iqbal <a1950138@ingb16w029.ad.adelaide.edu.au>
2025-09-03 18:58:31 +0930

    prac5

--- main.cpp
+++ main.cpp
@@ -3,14 +3,13 @@
 #include "RecursiveBinarySearch.h"
 
 int main(){
-    vector<int> list = {3,2,4,5,6,7,9,8,6,5,6,7,8,5,3,67,8,56,32,42,66675,532,4,321};
+    vector<int> list;
+
     QuickSort quick;
     vector<int> temp = quick.sort(list);
-    if(RecursiveBinarySearch::search(temp, 1)){
-        cout << "true ";
-    } else {
-        cout << "false ";
-    }
+
+    cout << (RecursiveBinarySearch::search(temp, 1)? "true " : "false ");
+    
     for ( int i: temp){
         cout << i << " "; 
     }
