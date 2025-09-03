7870a6037ff0d59438d3b8f5453cc96c96076e2a
Suprito Iqbal <a1950138@ingb16w029.ad.adelaide.edu.au>
2025-09-03 19:01:52 +0930

    prac5

--- main.cpp
+++ main.cpp
@@ -1,15 +1,20 @@
 #include "BubbleSort.h"
 #include "QuickSort.h"
 #include "RecursiveBinarySearch.h"
+#include <sstream>
 
 int main(){
-    vector<int> list;
+    std::vector<int> list;
+    std::string input;
+    std::getline(std::cin, input);
+    std::istringstream iss(input);
+    for (int x; iss >> x;) list.push_back(x);
 
     QuickSort quick;
     vector<int> temp = quick.sort(list);
 
     cout << (RecursiveBinarySearch::search(temp, 1)? "true " : "false ");
-    
+
     for ( int i: temp){
         cout << i << " "; 
     }
