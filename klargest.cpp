b98d93d796635c8fdc083cfa0f2e89075b51a949
Suprito <a1950138@adelaide.edu.au>
2025-10-22 07:49:12 +1030

    prac8

--- klargest.cpp
+++ klargest.cpp
@@ -1,6 +1,20 @@
-#include"Heap.tpp"
+#include<queue>
+#include <vector>
+#include <cmath>
 using namespace std;
 
 int kth_largest(vector<int> values, int k){
-    return 0;
+    if (k>values.size()){
+        return NULL;
+    }
+    priority_queue<int, vector<int>, greater<int>> priorityValues;
+    for (int element : values) {
+        if (priorityValues.size()< k) {
+            priorityValues.push(element);
+        } else if (element > priorityValues.top()) {
+            priorityValues.pop();
+            priorityValues.push(element);
+        }
+    }
+    return priorityValues.top();
 }
